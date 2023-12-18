0ad8f0a7ed138f5e994fcefbf82b0390fd0a28e5
prashantdh28 <123721861+prashantdh28@users.noreply.github.com>
2023-12-18 17:29:41 +0530

    Update TestController.java

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -12,8 +12,8 @@ public class TestController {
 	@GetMapping
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
-		object.put("name", "Integration Ninjas");
-		object.put("email", "integrationninjas@gmail.com");
+		object.put("name", "Prashant Dhole");
+		object.put("email", "prashantdhole.pd@gmail.com");
 		return object;
 	}
 
